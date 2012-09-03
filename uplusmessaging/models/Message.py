83709396b41d5d1095f2a6d7ff20265c87f09d90
Aleksandar Rodić <alec.rodic@gmail.com>
2012-09-03 15:32:08 +0200

    fixed num of messages per page in inbox/outbox

--- uplusmessaging/models/Message.py
+++ uplusmessaging/models/Message.py
@@ -8,11 +8,21 @@ class Message(db.Model):
     timestamp = db.DateTimeProperty(auto_now_add=True)
 
     @staticmethod
-    def received(username):
-        return Message.gql("WHERE receiver=:1", username)
+    def received(username, offset, limit):
+        return Message.fetch_messages("receiver", username, offset, limit)
 
     @staticmethod
-    def sent(username):
-        return Message.gql("WHERE sender=:1", username)
+    def sent(username, offset, limit):
+        return Message.fetch_messages("sender", username, offset, limit)
 
-
+    @staticmethod
+    def fetch_messages(role, username, offset, limit):
+        # if you change difference between offset and limit here 
+        # do the same in temp.js in function getPreviousMessages
+        offset = offset or 0
+        limit  = limit  or 10
+        offset, limit = int(offset), int(limit)
+        q = db.Query(Message)
+        q.filter(role, username)
+        q.order("-timestamp")
+        return q.fetch(offset=offset, limit=limit)
