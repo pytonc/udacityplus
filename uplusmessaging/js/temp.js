e120e93c3c9c8367b6e4fbddedeec6510fc9b859
Aleksandar Rodić <alec.rodic@gmail.com>
2012-09-09 17:37:59 +0200

    Button to newer msgs in inbox/outbox added

--- uplusmessaging/js/temp.js
+++ uplusmessaging/js/temp.js
@@ -1,4 +1,4 @@
-// Used by inbox/outbox
+// Used by inbox/outbox - refactoring needed
 
 function getPreviousMessages() {
     var url, from, to, m, temp;
@@ -12,7 +12,25 @@ function getPreviousMessages() {
         temp = to;
         to = 2 * to - from;
         from = temp;
-        url = m[1] + "from=" + from + "&to=" + to;
+        if(from >= 0 && to > 0 && to > from) {
+            url = m[1] + "from=" + from + "&to=" + to;
+        }
     }
     window.location.href = url
 };
+
+function getNewerMessages() {
+    var url, from, to, m, temp;
+    url = document.URL;
+    m = url.match(/(.+)from=(\d+)&to=(\d+)/);
+    from = parseInt(m[2], 10);
+    to = parseInt(m[3], 10);
+    temp = from;
+    from = 2 * from - to;
+    to = temp;
+
+    if(from >= 0 && to > 0 && to > from) {
+        url = m[1] + "from=" + from + "&to=" + to;
+        window.location.href = url
+    }
+}
