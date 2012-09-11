c276687a3250e17217509a5cd8b19e3efe046feb
jzegan <jzegan@gmail.com>
2012-09-11 03:06:45 +0200

    key-only queries for Message, added MessageIndex, got rid of fetch_messages, no longer similar query

--- uplusmessaging/models/Message.py
+++ uplusmessaging/models/Message.py
@@ -1,28 +1,34 @@
 from google.appengine.ext import db
 
+
 class Message(db.Model):
-    sender    = db.StringProperty(required=True)
-    receiver  = db.StringProperty(required=True)
-    title     = db.StringProperty(required=True)
-    content   = db.TextProperty(required=True)
-    timestamp = db.DateTimeProperty(auto_now_add=True)
+    # TODO: does receivers here badly affect performance?
+    receivers   = db.StringListProperty(required=True)
+    sender      = db.StringProperty(required=True)
+    title       = db.StringProperty(required=True)
+    content     = db.TextProperty(required=True)
+    timestamp   = db.DateTimeProperty(auto_now_add=True)
 
     @staticmethod
     def received(username, offset, limit):
-        return Message.fetch_messages("receiver", username, offset, limit)
+        offset = offset or 0
+        limit  = limit  or 10
+        offset, limit = int(offset), int(limit)
+
+        keys = db.GqlQuery(
+            "SELECT __key__ FROM MessageIndex WHERE receivers = :1", username
+        ).fetch(limit, offset)
+        return db.get([k.parent() for k in keys])
 
     @staticmethod
     def sent(username, offset, limit):
-        return Message.fetch_messages("sender", username, offset, limit)
-
-    @staticmethod
-    def fetch_messages(role, username, offset, limit):
-        # if you change difference between offset and limit here 
-        # do the same in temp.js in function getPreviousMessages
         offset = offset or 0
         limit  = limit  or 10
         offset, limit = int(offset), int(limit)
         q = db.Query(Message)
-        q.filter(role, username)
+        q.filter('sender', username)
         q.order("-timestamp")
         return q.fetch(offset=offset, limit=limit)
+
+class MessageIndex(db.Model):
+    receivers   = db.StringListProperty(required=True)
