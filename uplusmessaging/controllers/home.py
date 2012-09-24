8011b8376dc48606f070dbce803f07eed9ff4563
jzegan <jzegan@gmail.com>
2012-09-24 18:02:19 +0200

    check authentication, clean up some

--- uplusmessaging/controllers/home.py
+++ uplusmessaging/controllers/home.py
@@ -1,13 +1,19 @@
 from BaseHandler import *
+from controllers.helpers.authentication import Authentication
 from models.User import User
 
 class HomePage(BaseHandler):
+    @Authentication.do
     def get(self):
         #TODO: add sanity
         name = self.get_cookie('username')
-        user = User.get_user(name)
+        friends = None
 
-        template_values = {'title' : self.get_title(), 'username': name, 'friends': user.get_friends()}
+        if name:
+            user = User.get_user(name)
+            friends = user.get_friends()
+
+        template_values = {'title' : self.get_title(), 'username': name, 'friends': friends}
         self.render("index.html", template_values)
 
     def get_title(self):
