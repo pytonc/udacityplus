c8ea48466b68beafd3c6e59743c1029206e93e44
jzegan <jzegan@gmail.com>
2012-09-24 17:11:28 +0200

    added

---
+++ uplusmessaging/controllers/profile.py
@@ -0,0 +1,43 @@
+from webapp2 import uri_for
+from BaseHandler import *
+from jinja_custom.helpers import get_gravatar
+from models.User import User
+
+class ProfilePage(BaseHandler):
+    def get(self, username):
+        """display profile of user with username, if None, display logged in user
+        """
+        if not username:
+            username = self.get_cookie("username")
+
+        gravatar = None
+        friends = None
+        user = User.get_user(username)
+        if user:
+            # this is for link to logged in user's profile
+            username = self.get_cookie("username")
+            me = User.get_user(username)
+            friends = me.get_friends()
+
+            if user.use_gravatar:
+                if not user.avatar_url:
+                    gravatar = get_gravatar(user.email)
+                    user.avatar_url = gravatar
+                    user.put()
+                else:
+                    gravatar = user.avatar_url
+            else:
+                gravatar = user.avatar_url
+
+    #        uri_add = uri_for('friends-controller', friend=user.username_norm, action='add')
+
+
+            context = {'user': user, 'username': username, 'gravatar': gravatar, 'friends': friends}
+
+            self.render("profile/profile.html", context)
+        else:
+
+            self.redirect('/logout')
+
+    def post(self):
+        pass
