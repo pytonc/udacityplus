e37cfd8a71b28ecd292c6254dc81dc33e8852941
jzegan <jzegan@gmail.com>
2012-09-24 17:12:05 +0200

    querying for user to get friends/pass to template

--- uplusmessaging/controllers/home.py
+++ uplusmessaging/controllers/home.py
@@ -1,9 +1,13 @@
 from BaseHandler import *
+from models.User import User
 
 class HomePage(BaseHandler):
     def get(self):
+        #TODO: add sanity
         name = self.get_cookie('username')
-        template_values = {'title' : self.get_title(), 'username': name}
+        user = User.get_user(name)
+
+        template_values = {'title' : self.get_title(), 'username': name, 'friends': user.get_friends()}
         self.render("index.html", template_values)
 
     def get_title(self):
