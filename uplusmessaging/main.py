c30504521aea90a330b390354589879e5bb000e3
jzegan <jzegan@gmail.com>
2012-09-24 17:17:54 +0200

    add routes for FriendsController and ProfilePage

--- uplusmessaging/main.py
+++ uplusmessaging/main.py
@@ -1,17 +1,21 @@
 import webapp2
+from controllers.friends import FriendsController
 from controllers.home      import HomePage
 from controllers.signup    import SignUpPage
 from controllers.login     import LoginPage
 from controllers.logout    import LogoutPage
 from controllers.forbidden import Forbidden
 from controllers.messages  import MessagePage
+from controllers.profile import ProfilePage
 
 app = webapp2.WSGIApplication([
-        ('/', HomePage),
-        ('/signup', SignUpPage),
-        ('/login', LoginPage),
-        ('/logout', LogoutPage),
-        ('/forbidden', Forbidden),
-        ('/messages.*', MessagePage)
+        (r'/', HomePage),
+        (r'/signup', SignUpPage),
+        (r'/login', LoginPage),
+        (r'/logout', LogoutPage),
+        (r'/forbidden', Forbidden),
+        (r'/messages.*', MessagePage),
+        (r'/friends', FriendsController),
+        (r'/(?P<username>\w+)', ProfilePage),
     ], debug=True)
 
