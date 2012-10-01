76839acec12ff2454b919a0cd06afdca31572a9d
jzegan <jzegan@gmail.com>
2012-10-01 02:28:46 +0200

    proper 404 if looking at profile that doesn't exist removed redundant code

--- uplusmessaging/controllers/profile.py
+++ uplusmessaging/controllers/profile.py
@@ -13,8 +13,6 @@ class ProfilePage(BaseHandler):
         if not username:
             username = self.get_cookie("username")
 
-        gravatar = None
-        friends = None
         user = User.get_user(username)
         if user:
             # this is for link to logged in user's profile
@@ -22,26 +20,16 @@ class ProfilePage(BaseHandler):
             me = User.get_user(username)
             friends = me.get_friends()
 
-            if user.use_gravatar:
-                if not user.avatar_url:
-                    gravatar = get_gravatar(user.email)
-                    user.avatar_url = gravatar
-                    user.put()
-                else:
-                    gravatar = user.avatar_url
-            else:
-                gravatar = user.avatar_url
-
             if friends:
                 friend_btn = user.username_norm not in (f.username_norm for f in friends)
             else:
                 friend_btn = True
-            context = {'user': user, 'username': username, 'gravatar': gravatar, 'friends': friends, 'friend_btn': friend_btn}
+            context = {'user': user, 'username': username, 'friends': friends, 'friend_btn': friend_btn}
 
             self.render("profile/profile.html", context)
         else:
+            self.abort(404)
 
-            self.redirect('/logout')
     @Authentication.do
     def post(self):
         pass
