186702171730c872f82f437d311ac0a02806346d
jzegan <jzegan@gmail.com>
2012-10-07 04:07:58 +0200

    removed import

--- uplusprofiles/models/Course.py
+++ uplusprofiles/models/Course.py
@@ -1,5 +1,5 @@
 from google.appengine.ext import  ndb
-from google.appengine.ext.ndb.key import Key
+
 
 class Source(ndb.Model):
     name        = ndb.StringProperty()
@@ -11,7 +11,7 @@ class Course(ndb.Model):
     dept        = ndb.StringProperty()
     cid         = ndb.ComputedProperty(lambda self: self.dept + str(self.number))
     name        = ndb.StringProperty(required=True)
-    level       = ndb.StringProperty(choices=['Beginning', 'Intermediate', 'Advanced', 'Other'])
+    level       = ndb.StringProperty(choices=('Beginning', 'Intermediate', 'Advanced', 'Other'))
     description = ndb.TextProperty()
 
 class CourseIndex(ndb.Model):
