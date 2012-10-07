ac63a72d6be3b94dad286a707713b633f73b388d
jzegan <jzegan@gmail.com>
2012-10-07 04:11:07 +0200

    edit option, passing dob to template

--- uplusprofiles/controllers/profile.py
+++ uplusprofiles/controllers/profile.py
@@ -3,6 +3,7 @@ from BaseHandler import *
 from controllers.helpers.authentication import Authentication
 from jinja_custom.helpers import get_gravatar
 from models.User import User
+from datetime import date, timedelta
 
 
 class ProfilePage(BaseHandler):
@@ -11,24 +12,35 @@ class ProfilePage(BaseHandler):
         """display profile of user with username, if None, display logged in user
         """
 
+        mode, = self.get_params(['mode'])
+
+
+        if mode != 'edit':
+            template = 'profile/profile.html'
+        else:
+            template = 'profile/edit.html'
+
         user = User.get_user(username)
         if not user:
             user = User.save(username, 'some long password', '{}@someplace.com'.format(username))
 
+
+        dob = user.created - 13 * timedelta(days=365)
 
         gravatar = user.avatar_url
         friends = []
 
         if user:
 
-            context = {'user': user, 'username': username, 'gravatar': gravatar, 'friends': friends, 'friend_btn': False}
+            context = {'user': user, 'dob': dob, 'username': username, 'gravatar': gravatar, 'friends': friends, 'friend_btn': False}
 
-            self.render("profile/profile.html", context)
+            self.render(template, context)
         else:
 
             self.redirect('/logout')
 
 
     #@Authentication.do
-    def post(self):
+    def post(self, username):
+
         pass
