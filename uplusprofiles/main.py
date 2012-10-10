13368539eb4b075324f337f378b462f599bd7882
jzegan <jzegan@gmail.com>
2012-10-10 17:30:47 +0200

    debug mode if development server and turn on logging

--- uplusprofiles/main.py
+++ uplusprofiles/main.py
@@ -1,7 +1,14 @@
 import webapp2
 from controllers.profile import ProfilePage
+import logging
+import os
+
+DEBUG = bool(os.environ['SERVER_SOFTWARE'].startswith('Development'))
+if DEBUG:
+    logging.getLogger().setLevel(logging.DEBUG)
+
 
 app = webapp2.WSGIApplication([
     (r'/(?P<username>\w+)', ProfilePage),
-    ], debug=False)
+    ], debug=DEBUG)
 
