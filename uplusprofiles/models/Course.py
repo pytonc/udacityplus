c02aae31357fe659ecdc7f1ea8293e0607ad1908
jzegan <jzegan@gmail.com>
2012-10-15 20:36:35 +0200

    courses to dictionary to memcache

--- uplusprofiles/models/Course.py
+++ uplusprofiles/models/Course.py
@@ -1,6 +1,9 @@
 from google.appengine.ext.ndb import Key
 from google.appengine.ext import  ndb
+from collections import OrderedDict
+from google.appengine.api import memcache
 
+LEVELS = ('Beginning', 'Intermediate', 'Advanced', 'Other')
 
 class Source(ndb.Model):
     name        = ndb.StringProperty()
@@ -11,7 +14,7 @@ class Course(ndb.Model):
     dept        = ndb.StringProperty()
     cid         = ndb.ComputedProperty(lambda self: self.dept + str(self.number))
     name        = ndb.StringProperty(required=True)
-    level       = ndb.StringProperty(choices=('Beginning', 'Intermediate', 'Advanced', 'Other'))
+    level       = ndb.StringProperty(choices=LEVELS)
     short_desc  = ndb.StringProperty()
     long_desc   = ndb.TextProperty()
     source      = ndb.KeyProperty(Source)
@@ -30,4 +33,28 @@ class Course(ndb.Model):
         kn = self.centid(source, kwargs['dept'], kwargs['number'])
         kwargs['number'] = int(kwargs['number'])
 
+        # remove if a new course added
+        memcache.delete('courses_by_level_cid', namespace='udacityplus')
+
         return Course.get_or_insert(kn, **kwargs)
+
+    @classmethod
+    def courses_to_dict(cls):
+        courses = memcache.get('courses_by_level_cid', namespace='udacityplus')
+        if courses is not None:
+            return courses
+        else:
+            crquery = Course.query().order(Course.cid)
+
+            courses = OrderedDict()
+            for level in LEVELS:
+                co = []
+                for cr in crquery:
+                    if cr.level == level:
+                        co.append(cr)
+                    courses[level] = co
+
+            # store for 3 weeks
+            memcache.add('courses_by_level_cid', courses, time=1814400, namespace='udacityplus')
+
+            return courses
