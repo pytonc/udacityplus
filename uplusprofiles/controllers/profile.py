9d0c20fe77aec7248ff823439a2b42c5b25980c3
jzegan <jzegan@gmail.com>
2012-10-19 00:09:07 +0200

    added post for profile saving, added some context values in get, added functions for cleaning submitted profile data

--- uplusprofiles/controllers/profile.py
+++ uplusprofiles/controllers/profile.py
@@ -1,11 +1,20 @@
+from google.appengine.ext.ndb import Key, get_multi
 from BaseHandler import *
 from jinja_custom.helpers import get_gravatar
 from models.Course import Course
 from models.User import User
 from datetime import date, timedelta
+import logging
 
 
 class ProfilePage(BaseHandler):
+    def organize_courses_for(self, user):
+        all =  user.get_all_courses()
+        cc = get_multi([c.course for c in all if c.completed == True])
+        ic = get_multi([c.course for c in all if c.completed == False])
+
+        return all, ic, cc
+
     #@Authentication.do
     def get(self, username):
         """display profile of user with username, if None, display logged in user
@@ -19,31 +28,102 @@ class ProfilePage(BaseHandler):
             template = 'profile/edit.html'
 
         user = User.get_user(username)
+
         if not user:
-            user = User.save(username, 'some long password', '{}@someplace.com'.format(username))
+            user = User.save(username, '{}@someplace.com'.format(username), 'some long password')
 
-
-        dob = user.created - 13 * timedelta(days=365)
 
         gravatar = user.avatar_url
         friends = []
 
-        courses = Course.query()
-
         if user:
+            all, ic, cc = self.organize_courses_for(user)
 
-            context = {'user': user, 'dob': dob,
+            if user.dob:
+                dob = user.dob.strftime('%m/%d/%Y')
+            else:
+                dob = None
+
+
+            context = {'user': user,
+                       'dob': dob,
                        'username': username,
                        'gravatar': gravatar,
                        'friends': friends,
                        'friend_btn': False,
-                       'courses': courses}
+                       'courses_all': Course.courses_to_dict(),
+                       'courses_completed': cc,
+                       'courses_incomplete': ic,
+                       'errors': {}}
 
             self.render(template, context)
         else:
             self.redirect('/logout')
 
+    def clean_user_data(self, user, **kwargs):
+        if not kwargs['password'] and not kwargs['password_confirm']:
+            del kwargs['password']
+            del kwargs['password_confirm']
+            p = {}
+        else:
+            _, p = User.valid_passwords(kwargs['password'], kwargs['password_confirm'])
+        _, e = User.valid_email(kwargs['email'], user)
+        _, b = User.valid_date(kwargs['dob'])
+
+
+        if user.email != kwargs['email']:
+            valid, e = User.valid_email(kwargs['email'])
+
+        #TODO: should probably rename this, it can add whatever 3 dictionaries
+        errors = User.adduep(p, e, b)
+
+        return errors
+
     #@Authentication.do
     def post(self, username):
+        fields = self.get_params_dict((
+            'real_name',
+            'email',
+            'short_about',
+            'dob',
+            'tools',
+            'password',
+            'password_confirm',
+            'notify_on_msg'
+            ))
 
-        pass
+        iclasses = self.request.get_all('classes_inprog')
+        cclasses = self.request.get_all('classes_completed')
+        fields['iclasses'] = iclasses
+        fields['cclasses'] = cclasses
+        fields['username'] = username
+
+        user = Key(User, username).get()
+
+        errors = self.clean_user_data(user, **fields)
+
+        context = {
+            'errors': errors,
+            'user': user
+        }
+
+        if not errors:
+            user.update(**fields)
+            self.redirect('/{}'.format(username))
+        else:
+
+            if user.dob:
+                dob = user.dob.strftime('%m/%d/%Y')
+            else:
+                dob = None
+            all, ic, cc = self.organize_courses_for(user)
+            context['courses_all'] = Course.courses_to_dict()
+            context['courses_completed'] = cc
+            context['courses_incomplete'] = ic
+            context['dob'] = dob
+            context['username'] = username
+            context['gravatar'] = user.avatar_url
+            context['friends'] = []
+            context['friend_btn'] = False
+            context['errors'] = errors
+            self.render('profile/edit.html'.format(username), context)
