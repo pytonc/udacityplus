390c2ee32f0a6e4eeb802a88e91fb29e29367bfa
jzegan <jzegan@gmail.com>
2012-11-11 00:53:23 +0100

    update test, validation functions return bool, error_string

--- uplusmessaging/tests/datastore/user_test.py
+++ uplusmessaging/tests/datastore/user_test.py
@@ -21,27 +21,27 @@ class TestUser(unittest.TestCase):
 
     def testValidUsername(self):
         for k, v in uv.iteritems():
-            u = User.valid_username(k)
+            u, err = User.valid_username(k)
             self.assertEqual(u, v, k)
 
     def testValidPassword(self):
         for k, v in pv.iteritems():
-            p = User.valid_password(k)
+            p, err = User.valid_password(k)
             self.assertEqual(p, v, k)
 
     def testValidEmail(self):
-        e = User.valid_email(email1)
+        e, err = User.valid_email(email1)
         self.assertEqual(e, True, "With no existing user")
         u = User.save(username1, email1, password1)
-        u.put()
-        e = User.valid_email(email1)
+#        u.put()
+        e, err = User.valid_email(email1)
         self.assertEqual(e, False, "After create user with the email being tested")
 
     def testValid(self):
         email = 'jzegan@gmail.com'
         for k, v in uv.iteritems():
             for p, t in pv.iteritems():
-                valid = User.valid(k, email, p)
+                valid, err = User.valid(k, email, p)
                 self.assertEqual(valid, v and t, "{} - {}".format(k, p))
 
     def testSave(self):
