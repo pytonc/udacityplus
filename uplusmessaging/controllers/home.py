289e44abb1f4c0a5f28130ae46160e1aba748eca
jzegan <jzegan@gmail.com>
2012-11-11 18:56:14 +0100

    fixed logout

--- uplusmessaging/controllers/home.py
+++ uplusmessaging/controllers/home.py
@@ -6,13 +6,16 @@ class HomePage(BaseHandler):
     def get(self):
         name = self.get_cookie('username')
         friends = None
+        template_values = {}
 
         if name:
             user = User.get_user(name)
-            if user:
+            if user and Authentication.valid_log_token(user.username_norm, user.log_token):
                 friends = user.get_friends()
+                template_values = {'title' : self.get_title(), 'username': name, 'friends': friends}
+            else:
+                self.redirect('/logout')
 
-        template_values = {'title' : self.get_title(), 'username': name, 'friends': friends}
         self.render("index.html", template_values)
 
     def get_title(self):
