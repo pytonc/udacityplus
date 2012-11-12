06cfd5cab8306faa4860e130a342db535a86eaf9
jzegan <jzegan@gmail.com>
2012-11-12 16:48:52 +0100

    Added search handler route

--- uplusmessaging/main.py
+++ uplusmessaging/main.py
@@ -1,12 +1,13 @@
 import webapp2
-from controllers.friends import FriendsController
-from controllers.home      import HomePage
-from controllers.signup    import SignUpPage
-from controllers.login     import LoginPage
-from controllers.logout    import LogoutPage
-from controllers.forbidden import Forbidden
-from controllers.messages  import MessagePage
-from controllers.profile import ProfilePage
+from controllers.friends    import FriendsController
+from controllers.home       import HomePage
+from controllers.signup     import SignUpPage
+from controllers.login      import LoginPage
+from controllers.logout     import LogoutPage
+from controllers.forbidden  import Forbidden
+from controllers.messages   import MessagePage
+from controllers.profile    import ProfilePage
+from controllers.usersearch import Search
 
 import logging
 import os
@@ -24,6 +25,7 @@ app = webapp2.WSGIApplication([
         (r'/forbidden', Forbidden),
         (r'/messages(?:/?)([0-9]*)(?:/?)([0-9]*)', MessagePage),
         (r'/friends', FriendsController),
+        (r'/search', Search),
         (r'/(?P<username>\w+)', ProfilePage),
     ], debug=DEBUG)
 
