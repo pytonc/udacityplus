9244f4b32d36e8e44bc389b6257ac78d8cf18c19
jzegan <jzegan@gmail.com>
2012-12-11 14:36:47 +0100

    Redirect routes, catchall (profile) route,

--- routes.py
+++ routes.py
@@ -12,7 +12,7 @@ from web.controllers import profile
 from web.controllers import friends
 from web.controllers import messages
 from web.controllers import usersearch
-from web.controllers import forbidden
+from web.controllers import usersettings
 
 
 secure_scheme = 'https'
@@ -20,19 +20,39 @@ secure_scheme = 'https'
 _routes = [
     RedirectRoute('/', home.UserHomePage, name='home', strict_slash=True),
 #    (r'/forbidden', forbidden.Forbidden),
-    (r'/messages(?:/?)([0-9]*)(?:/?)([0-9]*)', messages.MessagePage),
+    RedirectRoute(r'/messages', messages.MessagePage, name="messages", strict_slash=True),
+    RedirectRoute(r'/messages/<conv_id:[0-9]*>/<msg_id:[0-9]*>', messages.MessagePage, name="message", strict_slash=True),
     (r'/friends', friends.FriendsController),
-    (r'/search', usersearch.Search),
-    RedirectRoute('/<username:\w+>', profile.ProfilePage, name='profile', strict_slash=True),
+    RedirectRoute(r'/search', usersearch.Search, name="search", strict_slash=True),
+    RedirectRoute(r'/settings', usersettings.UserSettings, name="settings", strict_slash=True),
 
-#    RedirectRoute('/secure/', home.HomePage, name='secure', strict_slash=True),
+    RedirectRoute('/<username:\w+>/project/<mode:\w+>', profile.Projects,
+        name="project_view", strict_slash=True, handler_method='view_project', methods=('GET')),
+
+    RedirectRoute('/<username:\w+>/project/add', profile.ProjectsUpload,
+        name='project-add', strict_slash=True, handler_method='add_project', methods=('POST')),
+
+    RedirectRoute('/<username:\w+>/project/edit', profile.ProjectsUpload,
+        name='project-edit', strict_slash=True, handler_method='edit_project', methods=('POST')),
+
+    RedirectRoute('/<username:\w+>/project/delete', profile.Projects,
+        name='project-delete', strict_slash=True, handler_method='delete_project', methods=('POST')),
+
+    RedirectRoute('/', home.UserHomePage, name='home', strict_slash=True),
     ]
+
+_routes_catchall = [
+    RedirectRoute('/<username:\w+>', profile.ProfilePage, name='profile', strict_slash=True, methods=('GET, POST')),
+]
 
 def get_routes():
     return _routes
 
-def add_routes(app):
+def add_routes(app, routes=_routes):
     if app.debug:
         secure_scheme = 'http'
-    for r in _routes:
+    for r in routes:
         app.router.add(r)
+
+def add_routes_catchall(app):
+    add_routes(app, _routes_catchall)
