c35e4918eedb8b66678f9d6e0a4deca43e364fe9
jzegan <jzegan@gmail.com>
2012-12-11 15:02:52 +0100

    user search by post, with csrf, changed results formatting

--- static/js/user_lookup.js
+++ static/js/user_lookup.js
@@ -1,15 +1,22 @@
 function userFormatResult(person) {
     var markup = "<table class='psearch-result'><tr>";
-    markup += "<td class='psearch-avatar'><img src='" + person.gravatar + "'/></td>"
-    markup += "<td class='psearch-info'><div class='psearch-username'>"
-    markup += "<a href='" + person.username + "'>" + person.username + "</a></div>";
+    markup += "<td class='psearch-avatar'><img src='" + person.gravatar + "'/></td>";
+    markup += "<td class='psearch-info'>";
+//    markup += "<div class='psearch-username'><a href='" + person.username + "'>" + person.username + "</a></div>";
 
 
-    if (person.real_name !== undefined) {
-        markup += "<div class='psearch-realname'>" + person.real_name + "</div>";
+//    if (person.real_name !== undefined) {
+//        markup += "<div class='psearch-realname'>" + person.real_name + "</div>";
+//    }
+
+    if (person.real_name !== undefined && person.real_name) {
+        markup += "<div class='psearch-username'><a href='" + person.username + "'>" + person.real_name + "</a></div>";
+    }
+    else {
+        markup += "<div class='psearch-username'><a href='" + person.username + "'>" + person.username + "</a></div>";
     }
 
-    markup += "</td></tr></table>"
+    markup += "</td></tr></table>";
     return markup;
 }
 
@@ -18,16 +25,19 @@ function userFormatSelection(person) {
 }
 $(document).ready(function() {
     $("#search").select2({
-        placeholder: "Search for a Udacian",
+        placeholder: "Search for a people",
         minimumInputLength: 3,
         ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
+            type: 'POST',
             url: "/search",
             dataType: 'jsonp',
+            quietMillis: 30,
             data: function (term, page) {
                 return {
                     q: term, // search term
-                    page_limit: 10,
-                    apikey: "1234567890" // please do not use so this example keeps working
+                    page_limit: 15,
+                    apikey: "1234567890", // please do not use so this example keeps working
+                    _csrf_token: search_csrf_token
                 };
             },
             results: function (data, page) { // parse the results into the format expected by Select2.
